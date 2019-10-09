911c309a19a22d268282ddb4e399e2194b7abaa8
teerapat.lar <teerapat.lar@ngernturbo.com>
2019-10-09 15:35:01 +0700

    check token

--- ThaiPost/Controllers/ThaiPostController.cs
+++ ThaiPost/Controllers/ThaiPostController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ThaiPost.Models;
 using ThaiPost.Services;
+using ThaiPost.Validation;
 using static ThaiPost.Models.HookData;
 
 namespace ThaiPost.Controllers
@@ -28,6 +29,7 @@ namespace ThaiPost.Controllers
         [HttpPost("api/thai-post/hook-data")]
         public void PostHookData(HookDataRequest request)
         {
+            Request.CheckAuthorization();
             //เอา URL : api/thai-post/hook-data นี้ไปใส่ใน หน้า dashboard ของ https://track.thailandpost.co.th/dashboard# เวลาเค้ายิงกลับมาจะได้เข้า path นี้
             //เวลาสถานะวัสดุเปลี่ยนเค้าจะยิง request นี่มาบอกสถานะ
         }
---
+++ ThaiPost/Validation/Validation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using ThaiPost.ExceptionBase;
+
+namespace ThaiPost.Validation
+{
+    public static class Validation
+    {
+        public static void CheckAuthorization(this HttpRequest request)
+        {
+            var authorization = request.Headers["Authorization"];
+            if (!authorization.Equals("Bearer keyxxx"))
+            {
+                throw new ValidationException("Token ไม่ถูกต้อง");
+            }
+        }
+    }
+}
